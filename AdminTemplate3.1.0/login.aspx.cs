38dfaeaeb32bfd5d5d356023e6a141795bb548f8
2810yash <yash2810203@gmail.com>
2024-03-31 21:24:13 +0530

    Register and Login commit

--- AdminTemplate3.1.0/login.aspx.cs
+++ AdminTemplate3.1.0/login.aspx.cs
@@ -1,32 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web.UI.WebControls;
 
 namespace AdminTemplate3._1._0
 {
     public partial class login : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void TextBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void LinkButton1_Click(object sender, EventArgs e)
-        {
-
-        }
+        string strconn2 = ConfigurationManager.ConnectionStrings["strconn"].ConnectionString;
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            // Get the email and password from the textboxes
+            string email = TextBox1.Text.Trim();
+            string pass = TextBox2.Text.Trim();
 
+            // Call the signInUser method to authenticate the user
+            signInUser(email, pass);
+            
+        }
+
+        protected void signInUser(string email, string pass)
+        {
+            SqlConnection con = new SqlConnection(strconn2);
+            try
+            {
+                int result = 0;
+                int userID = 0;
+
+                using (con = new SqlConnection(strconn2))
+                {
+                    using (SqlCommand cmd = new SqlCommand("usp_login_tbl", con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@EmailID", email);
+                        cmd.Parameters.AddWithValue("@Password", pass);
+                        cmd.Parameters.Add("@Id", SqlDbType.Int).Direction = ParameterDirection.Output;
+
+                        con.Open();
+                        result = cmd.ExecuteNonQuery();
+
+                        // Get the output parameter value
+                        userID = (int)cmd.Parameters["@Id"].Value;
+
+                        con.Close();
+                    }
+                }
+
+                if (userID > 0)
+                {
+                    // Successful login
+                    Response.Write("<script>alert('Login Successfully.');</script>");
+                    // Redirect user to a dashboard page or any other page
+                    Response.Redirect("HomePage.aspx");
+                }
+                else
+                {
+                    // Unsuccessful login
+                    Response.Write("<script>alert('Login Unsuccessful.\nCheck your Details and Try Again');</script>");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
         }
     }
 }
--- AdminTemplate3.1.0/register.aspx.cs
+++ AdminTemplate3.1.0/register.aspx.cs
@@ -23,12 +23,22 @@ namespace AdminTemplate3._1._0
         // Sign Up Button
         protected void Button1_Click(object sender, EventArgs e)
         {
-            RecordInsert();
+            String email = TextBox1.Text.Trim();
+            String pass = TextBox2.Text.Trim();
+            String rePass = TextBox3.Text.Trim();
+
+            if (pass == rePass)
+            {
+                SignUpNewUser(email, pass);
+            }
+            else
+            {
+                Response.Write("<script>alert('PassWord does not matched');</script>");
+            }
         }
 
-        protected void RecordInsert()
+        protected void SignUpNewUser(String email, String pass)
         {
-            // Response.Write("<script>alert('Testing');</script>");
             SqlConnection con = new SqlConnection(strconn2);
             try
             {
@@ -38,22 +48,18 @@ namespace AdminTemplate3._1._0
                     using (SqlCommand cmd = new SqlCommand("usp_register_tbl", con))
                     {
                         cmd.CommandType = CommandType.StoredProcedure;
-                        cmd.Parameters.AddWithValue("@EmailID", TextBox1.Text.Trim());
-                        cmd.Parameters.AddWithValue("@Password", TextBox2.Text.Trim());
-                        //cmd.Parameters.AddWithValue("@RePassword", TextBox3.Text.Trim());
-                        //cmd.Parameters.AddWithValue("@AcountStatus", "pending");
+                        cmd.Parameters.AddWithValue("@EmailID", email);
+                        cmd.Parameters.AddWithValue("@Password", pass);
                         con.Open();
                         result = cmd.ExecuteNonQuery();
                         con.Close();
                         if (result > 0)
                         {
-                            Response.Write("Record inserted");
-                            Response.Write("<script>alert(Record Inserted);</script>");
+                            Response.Write("<script>alert('Register Successfully.\nGo for Login Page to Login.');</script>");
                         }
                         else
                         {
-                            Response.Write("Record not inserted");
-                            Response.Write("<script>alert(Record Not Inserted);</script>");
+                            Response.Write("<script>alert('Register UnSuccessfully.\nTry Again');</script>");
                         }
                     }
                 }
@@ -66,7 +72,7 @@ namespace AdminTemplate3._1._0
 
         protected void LinkButton1_Click(object sender, EventArgs e)
         {
-            
+            Response.Redirect("login.aspx");
         }
     }
 }
